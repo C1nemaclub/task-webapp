dfd5fb445a466550b446a3a8e135232ea311e131
C1nemaclub <santiagov801@gmail.com>
2024-05-14 20:36:15 -0500

    dashboard / teams

--- src/core/features/tasks/taskService.ts
+++ src/core/features/tasks/taskService.ts
@@ -4,7 +4,7 @@ import { Task } from '../../types/roles.model';
 export const getTasks = async () => {
   return await pb.collection('tasks').getFullList<Task>({ expand: 'asignee' });
   // return await pb.collection('tasks').getList(0, 20, {
-  //   filter: 'asignee = "igvnt6apq16svw4"',
+  //   filter: 'asignee = "ujygyjgeu3y69l8"',
   //   expand: 'asignee',
   // });
 };
--- src/core/features/teams/teamService.ts
+++ src/core/features/teams/teamService.ts
@@ -5,6 +5,10 @@ export const getTeams = async () => {
   return await pb.collection('teams').getFullList<Team>();
 };
 
+// const getTeamsByUser = (userId: string, start: number = 0, end: number = 10) => {
+//   return await pb.collection('teams').getList<Team>(start, end, { expand: {} });
+// };
+
 export const createTeam = async (teamName: string) => {
   return await pb.collection('teams').create<Team>({
     name: teamName,
